5598e6b8cd54d2cf88fff6435bbe402fcfcb590f
Nnadi Victory <chikamagozi25@gmail.com>
2024-05-28 11:45:39 +0100

    Update SearchLoadingUI.svelte

--- src/lib/components/SearchLoadingUI.svelte
+++ src/lib/components/SearchLoadingUI.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-row items-center gap-4">
-	<Skeleton class="h-16 w-16 rounded-full bg-gray-600" />
+	<Skeleton class="!h-16 !w-16 rounded-full bg-gray-600" />
 	<div class="w-[50%] space-y-2">
 		<Skeleton class="h-4 w-[40%] rounded-none bg-gray-600" />
 		<Skeleton class="h-4 w-[20%] rounded-none bg-gray-600" />
@@ -11,7 +11,7 @@
 	<Skeleton class=" top-1/2 ml-auto mr-5 h-9 w-[20%] bg-gray-600" />
 </div>
 <div class="flex flex-row items-center gap-4">
-	<Skeleton class="h-16 w-16 rounded-full bg-gray-600" />
+	<Skeleton class="!h-16 !w-16 rounded-full bg-gray-600" />
 	<div class="w-[50%] space-y-2">
 		<Skeleton class="h-4 w-[40%] rounded-none bg-gray-600" />
 		<Skeleton class="h-4 w-[20%] rounded-none bg-gray-600" />
